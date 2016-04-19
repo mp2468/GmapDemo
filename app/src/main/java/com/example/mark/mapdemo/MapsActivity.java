56930ea19b7fdde830cdd770eacbb64624072e34
mp2468 <pianomanmark@gmail.com>
2016-04-19 13:35:24 -0400

    getting my location, and addingfunctionality

--- app/src/main/java/com/example/mark/mapdemo/MapsActivity.java
+++ app/src/main/java/com/example/mark/mapdemo/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.mark.mapdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +16,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    private EditText latE, longE;
+    private Button submitLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        latE = (EditText) findViewById(R.id.LatField);
+        longE = (EditText) findViewById(R.id.LongField);
+        submitLoc = (Button) findViewById(R.id.subButton);
+
+        submitLoc.setOnClickListener(this);
     }
 
     /*
@@ -47,9 +61,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+//https://developers.google.com/maps/documentation/android-api/location#the_my_location_layer
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-//        LatLng Baltimore = new LatLng();
+        LatLng Baltimore = new LatLng(39, -76);
 
 
 
@@ -58,4 +85,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
